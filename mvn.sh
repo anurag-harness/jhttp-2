18237d83184c986c0b90978c94672a1842e02f34
Anurag Madnawat <anurag.madnawat@harness.io>
2023-02-28 15:56:27 -0800

    test

--- mvn.sh
+++ mvn.sh
@@ -1,7 +1,7 @@
-CG_JSON_FILE="/tmp/callgraph/jhttp/cg/coverage*.json"
+CG_JSON_FILE="/Users/anurag/java-agent-test/jhttp/cg/coverage*.json"
 
-JAVA_AGENT="/Users/shiv/.m2/repository/software/wings/980-java-agent/0.0.1-SNAPSHOT/980-java-agent-0.0.1-SNAPSHOT.jar"
-CONFIG_FILE="/Users/shiv/git/jhttp/config.ini"
+JAVA_AGENT="/Users/anurag/java-agent-test/java-agent.jar"
+CONFIG_FILE="/Users/anurag/java-agent-test/config.ini"
 
 
 export MAVEN_OPTS="-javaagent:$JAVA_AGENT=$CONFIG_FILE"
